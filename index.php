0889a83b99750f23e0ab5f909bd18f39f5b616cd
Matthew Spire <matthew.t.spire@gmail.com>
2023-10-29 11:44:58 -0400

    Variables lesson

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $greeting = "Hi";
+
+            echo "$greeting Everybody!";
+        ?>
+    </h1>
+</body>
+</html>
